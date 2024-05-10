678df4875892fed47428b9537929ce09894183d9
Olu <oluseunamao@gmail.com>
2024-05-10 16:42:44 -0500

    Updated CORS and requirements.txt

--- .venv/app.py
+++ .venv/app.py
@@ -1,25 +1,27 @@
-from services.get_data import get_news
-from flask import Flask,render_template
 
-from flask import Flask
+from flask import Flask,render_template
+from services.get_data import get_news
+from flask_cors import CORS
+
+
+
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/news", methods=['GET'])
 def get_data():
     return get_news()
 
-@app.route("/doc")
+
+@app.route("/docs")
 def render_docs():
     return render_template("docs.html")
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0',debug=True)
